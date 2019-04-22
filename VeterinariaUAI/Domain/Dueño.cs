f3b0ae9e20403ade4b9e310267836eeec718d3ab
Sebastian Caldarola <sebastian.caldarola@mercadolibre.com>
2019-04-21 21:18:06 -0300

    Update Dueño.cs

--- VeterinariaUAI/Domain/Dueño.cs
+++ VeterinariaUAI/Domain/Dueño.cs
@@ -6,5 +6,30 @@ namespace VeterinariaUAI.Domain
         public Dueño()
         {
         }
+        
+        public Dueño(string nombre, string apellido, string dni)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Dni = dni;
+        }
+
+        public string Nombre
+        {
+            get;
+            private set;
+        }
+
+        public string Apellido
+        {
+            get;
+            private set;
+        }
+
+        public string Dni
+        {
+            get;
+            private set;
+        }
     }
 }
