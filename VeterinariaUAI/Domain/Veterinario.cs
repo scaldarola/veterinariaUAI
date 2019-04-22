99ece8a33810e365da7a80ccf1f0d4a205c642fb
Sebastian Caldarola <sebastian.caldarola@mercadolibre.com>
2019-04-21 21:17:44 -0300

    Update Veterinario.cs

--- VeterinariaUAI/Domain/Veterinario.cs
+++ VeterinariaUAI/Domain/Veterinario.cs
@@ -3,8 +3,36 @@ namespace VeterinariaUAI.Domain
 {
     public class Veterinario
     {
-        public Veterinario()
+        public Veterinario(string nombre, string apellido, string dni, string matricula)
         {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Dni = dni;
+            this.Matricula = matricula;
+        }
+
+        public string Nombre
+        {
+            get;
+            private set;
+        }
+
+        public string Apellido
+        {
+            get;
+            private set;
+        }
+
+        public string Dni
+        {
+            get;
+            private set;
+        }
+
+        public string Matricula
+        {
+            get;
+            private set;
         }
     }
 }
