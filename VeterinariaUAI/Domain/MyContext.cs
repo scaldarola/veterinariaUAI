89a1344a7bfcea4aeb1546f1dba5184c5db30591
scaldarola <sebastian.caldarola@mercadolibre.com>
2019-04-22 09:41:19 -0300

    added EF Context

---
+++ VeterinariaUAI/Domain/MyContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace VeterinariaUAI.Domain
+{
+    public class MyContext : DbContext
+    {
+        public virtual DbSet<Turno> Turnos { get; set; }
+        public virtual DbSet<Paciente> Pacientes { get; set; }
+        public virtual DbSet<Dueño> Dueños { get; set; }
+        public virtual DbSet<Sala> Salas { get; set; }
+        public virtual DbSet<Veterinario> Veterinarios { get; set; }
+    }
+}
