2c6bce25d04af302e478df134d06074db301cd8c
Sebastian Caldarola <sebastian.caldarola@mercadolibre.com>
2019-04-21 21:18:30 -0300

    Update Paciente.cs

--- VeterinariaUAI/Domain/Paciente.cs
+++ VeterinariaUAI/Domain/Paciente.cs
@@ -6,5 +6,37 @@ namespace VeterinariaUAI.Domain
         public Paciente()
         {
         }
+        
+        public Paciente(string nombre, DateTime fechanac, string tipo, Dueño dueño)
+        {
+            this.Nombre = nombre;
+            this.FechaNacimiento = fechanac;
+            this.TipoMascota = tipo;
+            this.DueñoMascota = dueño;
+        }
+
+        public string Nombre
+        {
+            get;
+            private set;
+        }
+
+        public DateTime FechaNacimiento
+        {
+            get;
+            private set;
+        }
+
+        public string TipoMascota
+        {
+            get;
+            private set;
+        }
+
+        public Dueño DueñoMascota
+        {
+            get;
+            private set;
+        }
     }
 }
